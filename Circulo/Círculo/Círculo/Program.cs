a1e0c07a09b796e28b62fcc2550143d86481124d
Oscar Roman <oscarroman.pe@gmail.com>
2023-03-25 00:13:56 -0500

    viriable's contents erased

---
+++ Circulo/Círculo/Círculo/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+var radio = 0d; // 0d indica que es un número con decimales
+var resultado = 0d;
+
+Console.WriteLine("Bienvenido, calculemos círculos!!");
+// Pi * radio * radio
+Console.WriteLine("Ingrese el radio");
+radio = Convert.ToDouble(Console.ReadLine());
+
+resultado = Math.PI * radio * radio;
+
+Console.WriteLine("El área de tu círculo es de: " + resultado);
---
+++ helloWorld_Platzi/Hello World Platzi/Hello World Platzi/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("¡Hola, estudiante Platzi!");
+
+// Programa que calcula el área de un rectángulo
+var ladoA = 1.1;
+var ladoB = 1.1;
+var resultado = 1.1;
+
+Console.WriteLine("Calcule el área de un rectángulo");
+Console.WriteLine("Ingrese el valor del lado A");
+ladoA = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el valor del lado B");
+ladoB = Convert.ToDouble(Console.ReadLine());
+
+resultado = ladoA * ladoB;
+Console.WriteLine("El lado A es: " + ladoA + "; el lado B es: " + ladoB + "; el resultado es " + resultado);
